4dcb08e6f5454ba83306ee4c32a4c40cbb1a20d7
Andreas Lind Petersen <andreas@one.com>
2013-09-16 23:41:58 +0200

    Added getTreeish helper that supports branches, tags, and commits.

--- test/GitFakeFs.js
+++ test/GitFakeFs.js
@@ -4,14 +4,14 @@ var expect = require('unexpected'),
     GitFakeFs = require('../lib/GitFakeFs');
 
 describe('GitFakeFs', function () {
-    var fsGit;
+    var gitFakeFs;
     beforeEach(function () {
-        fsGit = new GitFakeFs(Path.resolve(__dirname, 'testrepo.git'));
+        gitFakeFs = new GitFakeFs(Path.resolve(__dirname, 'testrepo.git'));
     });
 
     describe('#readdir()', function () {
         it('should list the files in the most recent commit', function (done) {
-            fsGit.readdir('/', passError(done, function (results) {
+            gitFakeFs.readdir('/', passError(done, function (results) {
                 expect(results, 'to equal', ['foo.txt']);
                 done();
             }));
@@ -19,7 +19,7 @@ describe('GitFakeFs', function () {
     });
 
     it('readFile("/foo.txt") should produce the most recent version of foo.txt as a buffer', function (done) {
-        fsGit.readFile('/foo.txt', passError(done, function (buf) {
+        gitFakeFs.readFile('/foo.txt', passError(done, function (buf) {
             expect(buf, 'to be a', Buffer);
             expect(buf.toString('utf-8'), 'to equal', 'This is the second revision of foo.txt\n\nIt also has non-ASCII chars: æøÅ\n\nAnd some more text...\n');
             done();
@@ -27,10 +27,25 @@ describe('GitFakeFs', function () {
     });
 
     it('readFile("/foo.txt", "utf-8") should produce the most recent version of foo.txt as a string', function (done) {
-        fsGit.readFile('/foo.txt', 'utf-8', passError(done, function (str) {
+        gitFakeFs.readFile('/foo.txt', 'utf-8', passError(done, function (str) {
             expect(str, 'to be a string');
             expect(str, 'to equal', 'This is the second revision of foo.txt\n\nIt also has non-ASCII chars: æøÅ\n\nAnd some more text...\n');
             done();
         }));
+    });
+
+
+    describe('pointed at the first commit in testrepo.git', function () {
+        var gitFakeFs;
+        beforeEach(function () {
+            gitFakeFs = new GitFakeFs(Path.resolve(__dirname, 'testrepo.git'), {ref: '738876c70f4f5243a6672def4233911678ce38db'});
+        });
+
+        it('should contain the initial version of foo.txt', function (done) {
+            gitFakeFs.readFile('/foo.txt', 'utf-8', passError(done, function (contents) {
+                expect(contents, 'to equal', 'This is the first revision of foo.txt\n\nIt has non-ASCII chars: æøÅ\n');
+                done();
+            }));
+        });
     });
 });
